816eb7e512e6f2c2663b12be3a90bb91e934a946
djruiz <djruiz@iu.edu>
2019-10-13 20:36:26 -0400

    JavaScript tutorial; covered loops and object oriented concepts

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ console.log(Array.isArray(numbers));
 
 console.log(numbers.indexOf(5));
 
-//Objects
+//Object literals
 const person = {
   firstName: "Bob",
   lastname: "Bobson",
@@ -89,3 +89,123 @@ console.log(todos[1].text);
 
 const todoJSON = JSON.stringify(todos);
 console.log(todoJSON);
+
+// For loop
+for (let i = 0; i < 10; i++) {
+  console.log(`For loop current i: ${i}`);
+}
+
+//While loop
+let i = 0;
+while (i < 10) {
+  console.log(`While loop number: ${i}`);
+  i++;
+}
+
+//Loop thorough an array
+
+//manual for loop
+for (let i = 0; i < todos.length; i++) {
+  console.log(todos[i].text);
+}
+
+//for loop with assigned variable for entries
+for (let todo of todos) {
+  console.log(todo.text);
+}
+
+//high order array functions
+//for each
+todos.forEach(function(todo) {
+  console.log(todo.text);
+});
+
+//map function
+const todoText = todos.map(function(todos) {
+  return todos.text;
+});
+
+console.log(todoText);
+
+//filter
+const todoId = todos.filter(function(todos) {
+  return todos.id;
+});
+
+console.log(todoId);
+
+//conditional statements
+const a = "10";
+//double equal does not check data type as well
+//triple equal will check the data type and would not log to the console
+if (a == 10) {
+  console.log("x is 10");
+} else if (a > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is less than 10");
+}
+
+//switch case
+const color = "black";
+switch (color) {
+  case "red":
+    console.log("Color is red");
+    break;
+  case "blue":
+    console.log("Color is blue");
+    break;
+  default:
+    console.log("Color is not blue or red");
+    break;
+}
+
+//functions
+//can declare default values for parameters
+function addNums(num1 = 1, num2 = 1) {
+  return num1 + num2;
+}
+
+console.log(addNums());
+
+//arrow functions
+
+const subtractNums = (num1 = 1, num2 = 0) => {
+  return num1 - num2;
+};
+
+console.log(subtractNums(3, 1));
+
+//Object Oriented programming in JavaScipt
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = dob;
+
+  this.sayHello;
+  this.getFullName = () => `${this.firstName} ${this.lastName}`;
+}
+
+//prototyping
+Person.prototype.sayHello = function() {
+  return `${this.firstName} says hello!`;
+};
+const Bob = new Person("Bob", "Bobsob", 1991);
+console.log(Bob);
+
+console.log(Bob.sayHello());
+console.log(Bob.getFullName());
+
+//creating classes
+//easier to read syntax; added in ES6
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+  }
+
+  sayHi() {
+    return `${this.firstName} says hello!`;
+  }
+}
